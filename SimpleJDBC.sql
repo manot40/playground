02a387d5205e2639e951ffa6f7e030ba0f93ff47
manot40 <kevinsandiho290100@gmail.com>
2020-07-26 20:51:41 +0700

    Update SimpleJDBC.sql

--- SimpleJDBC.sql
+++ SimpleJDBC.sql
@@ -19,3 +19,11 @@ CREATE TABLE IF NOT EXISTS `books` (
   `qty` int(11) DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DELETE FROM `books`
+INSERT INTO `books` (`ID`, `title`, `author`, `price`, `qty`) VALUES
+	(1001, 'Java for Dummies', 'Tan Ah Teck', 11.11, 11),
+	(1002, 'More Java for dummies', 'Teh Ah Teck', 22.22, 22),
+	(1003, 'More Java for more Dummies', 'Mohammad Ali', 33.33, 33);
+	(1004, 'A Cup of Java', 'Mohammad Ali', 44.44, 44);
+	(1005, 'A Teaspoon of Java', 'Mohammad Ali', 55.55, 55);
